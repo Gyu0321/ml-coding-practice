caaefa2b3bfce949ef7ffcbef9c640ad2fa1617e
최연규 <vkdlsjf069876@gmail.com>
2026-03-09 16:32:19 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -218,3 +218,22 @@ for key, value in dic.items():
 dic.clear()
 print(dic)
 
+# 집합 만들기
+s1 = {1, 2, 'a', 5}
+s2 = set([1, 2, 3, 4, 5, 6])
+print(s2)
+s3 = set([4, 5, 6, 7, 8, 9])
+print(s3)
+
+# 교집합 연산
+print(s2 & s3)
+print(s2.intersection(s3))
+
+# 합집합 연산
+print(s2 | s3)
+print(s2.union(s3))
+
+# 차집합 연산
+print(s2 - s3)
+print(s3 - s2)
+print(prints2.diff)
