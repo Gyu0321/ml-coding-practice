67679e66d3829afa929803947130d108212621fc
최연규 <vkdlsjf069876@gmail.com>
2026-03-09 16:33:49 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -236,4 +236,19 @@ print(s2.union(s3))
 # 차집합 연산
 print(s2 - s3)
 print(s3 - s2)
-print(prints2.diff)
+print(s2.difference(s3))
+print(s3.difference(s2))
+
+# 원소 한 개 추가
+s2.add(7)
+print(s2)
+
+# 원소 여러 개 추가
+s2.update([6, 7, 8, 9, 10])
+print(s2)
+
+# 특정 원소 제거
+s2.remove(7)
+print(s2)
+
+s2 = set([1, 2, 3, 4, 5, 6, 3, 1, 6])
