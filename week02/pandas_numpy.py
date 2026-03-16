b068b1094b4887bb4e520045e387be3005fd09d6
최연규 <vkdlsjf069876@gmail.com>
2026-03-16 22:52:21 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -197,4 +197,6 @@ print(np_a - np_b)
 print(np_a * np_b)
 print(np_a @ np_b)
 
-print(np.random.randint())
+print(np.random.randint(11))
+print(np.random.randint(50, 71))
+print(np.random.randint(50, 71, 10))
