333fe9558f7c565cafe581800a3bde53dc101acc
최연규 <vkdlsjf069876@gmail.com>
2026-03-23 13:21:52 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -178,3 +178,15 @@ plt.fill_between(survived_counts.index.astype(str), survived_counts[1],
 plt.fill_between(survived_counts.index.astype(str), survived_counts[0],
                  color='hotpink', alpha=0.6, label='Not Survived')
 
+plt.title('Survival by Age Group on Titanic')
+plt.xlabel('Age')
+plt.ylabel('Count')
+plt.xticks(rotation=45)
+plt.legend()
+plt.grid(True)
+plt.savefig('Figure08.png')
+plt.close()
+
+## **박스 플롯 : 승객 나이의 데이터 분포, 중앙값, 이상치 살펴보기**
+
+# 
