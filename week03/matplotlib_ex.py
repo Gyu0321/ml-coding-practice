d41417476b827857dde78b5ebba6c58330d4f99e
최연규 <vkdlsjf069876@gmail.com>
2026-03-23 13:10:52 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -143,4 +143,8 @@ titanic = titanic.dropna(subset=['Age', 'Fare'])
 correlation_matrix = titanic.drop('PassengerId', axis=1).corr(numeric_only=True)
 print(correlation_matrix)
 
-# 히트맵 그
+# 히트맵 그리기
+plt.matshow(correlation_matrix, cmap='PuRd_r')
+plt.colorbar()
+
+# x축과 y축의 눈금 설정
