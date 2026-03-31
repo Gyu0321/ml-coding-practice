9ec84075d74e52563bcef239484122608ee6f9e9
최연규 <vkdlsjf069876@gmail.com>
2026-03-31 13:29:33 +0900

    Updated Changes

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -80,4 +80,5 @@ plt.savefig('./week04/Seaborn_Figure05.jpg')
 # jointplot( ) 그리기
 sns.jointplot(data=tips, hue='sex', diag_kind='hist', palette='husl')
 
-plt.
+plt.suptitle('Pairplot with Histograms by Gender', y=1.05)
+plt.savefig('./week004/Seaborn_Figure06.jpg')
