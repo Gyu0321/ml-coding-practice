7100b1f81c8e2c09b1c6b5e1db9d4cd8b1e5ef54
최연규 <vkdlsjf069876@gmail.com>
2026-04-02 13:28:56 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -1,23 +1,10 @@
 # -*- coding: utf-8 -*-
 
 # 데이터 준비
-from pathlib import Path
 import numpy as np
 import pandas as pd
-import tarfile
-import urllib.request
 
-def load_housing_data():
-    tarball_path = Path("datasets/housing.tgz")
-    if not tarball_path.is_file():
-        Path("datasets").mkdir(parents=True, exist_ok=True)
-        url = "https://github.com/ageron/data/raw/main/housing.tgz"
-        urllib.request.urlretrieve(url, tarball_path)
-        with tarfile.open(tarball_path) as housing_tarball:
-            housing_tarball.extractall(path="datasets")
-    return pd.read_csv(Path("datasets/housing/housing.csv"))
-
-housing = load_housing_data()
+housing = pd.read_csv('./week04/housing.csv')       # 오류 발생 시, ./housing.csv 파일로도 시도
 
 # 테스트 세트 만들기
 from sklearn.model_selection import train_test_split
