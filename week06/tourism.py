8ceb29aee7ae802269cb54d572737556e89a88ed
최연규 <vkdlsjf069876@gmail.com>
2026-04-11 12:20:08 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -4,4 +4,12 @@ import datetime
 import json
 import pandas as pd
 
-ServiceKey = ""
+ServiceKey = "6514169a700526d7f6f93f1ac9600e34c7f1eeb0b1b7b51207f4d73cf5e8f776"
+
+"""### [CODE 0]"""
+
+def main():
+    jsonResult = []
+    result = []
+
+    print("<< 국내 ")
