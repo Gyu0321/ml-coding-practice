13718b95339fc976c9fe531b29c4961a578edf2a
최연규 <vkdlsjf069876@gmail.com>
2026-04-11 12:10:38 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -8,29 +8,29 @@ client_secret = 'Client Secret'
 
 def main():
 
-    node = 'news'                                           # 크롤링할 대상
+    node = 'news'                                       # 크롤링할 대상
     srcText = input('검색어를 입력하세요: ')
 
     cnt = 0
     jsonResult = []
 
-    jsonResponse = getNaverSearch(node, srcText, 1, 100)    # [CODE 2]
+    jsonResponse = getNaverSearch(node, srcText, 1, 100)     # [CODE 2]
     total = jsonResponse['total']
 
     while ((jsonResponse != None) and (jsonResponse['display'] != 0)):
         for post in jsonResponse['items']:
             cnt += 1
-            getPostData(post, jsonResult, cnt)          # [CODE 3]
+            getPostData(post, jsonResult, cnt)             # [CODE 3]
 
         start = jsonResponse['start'] + jsonResponse['display']
         jsonResponse = getNaverSearch(node. srcText, start, 100) # [CODE 2]
 
-    print('전체 검색 : #d 건' %total)
+    print('전체 검색 : %d 건' %total)
 
     with open('%s_naver_%s.json' % (srcText, node), 'w', encoding = 'utf8') as outfile:
         jsonFile = json.dumps(jsonResult, indent = 4, sort_keys = True,
                               ensure_ascii = False)
-        
+
         outfile.write(jsonFile)
 
     print("가져온 데이터 : %d 건" %(cnt))
