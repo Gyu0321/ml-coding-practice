19141f29bccf2bd88b714f16a3ce796532194abe
최연규 <vkdlsjf069876@gmail.com>
2026-04-11 12:37:08 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -58,4 +58,5 @@ def main():
 """### [CODE 2]"""
 
 def getTourismStatsItem(yyyymm, nat_cd, ed_cd):
-    service_url = ""
+    service_url = "http://openapi.tour.go.kr/openapi/service/EdrcntTourismStatsService/getEdrcntTourismStatsList"
+    parameters = "?_type"
