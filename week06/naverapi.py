324104bca0500b6981353f624f1d4fcbef65846b
최연규 <vkdlsjf069876@gmail.com>
2026-04-11 12:00:08 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -51,4 +51,12 @@ def getNaverSearch(node, srcText, page_start, display):
         return json.loads(responseDecode)
         
 
-def 
+def getRequestUrl(url):
+    req = urllib.request.Request(url)
+
+    req.add_header("X-Naver-Client-Id", client_id)
+    req.add_header("X-Naver-Client-Secret", client_secret)
+
+    try:
+        response = urllib.request.urlopen(req)
+        
