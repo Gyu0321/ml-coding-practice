dcf9406f40b6d5df1b33a7f34d394525f8edda40
최연규 <vkdlsjf069876@gmail.com>
2026-04-11 11:57:37 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -37,18 +37,18 @@ def main():
     print('%s_naver_%s.json SAVED' % (srcText, node))
 
 
-    def getNaverSearch(node, srcText, page_start, display):
-        base = "https://openapi.naver.com/v1/search"
-        node = "/%s.json" % node
-        parameters = "?query=%s&start=%s&display=%s" % (urllib.parse.quote(srcText), page_start, display)
+def getNaverSearch(node, srcText, page_start, display):
+    base = "https://openapi.naver.com/v1/search"
+    node = "/%s.json" % node
+    parameters = "?query=%s&start=%s&display=%s" % (urllib.parse.quote(srcText), page_start, display)
 
-        url = base + node +parameters
-        responseDecode = getRequestUrl(url)                 #[CODE 1]
+    url = base + node +parameters
+    responseDecode = getRequestUrl(url)                 #[CODE 1]
 
-        if (responseDecode == None):
-            return None
-        else:
-            return json.loads(responseDecode)
+    if (responseDecode == None):
+        return None
+    else:
+        return json.loads(responseDecode)
         
 
-    def
+def 
