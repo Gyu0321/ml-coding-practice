15de3afcaddc8314d687204206156802a8c2b0f2
최연규 <vkdlsjf069876@gmail.com>
2026-04-29 10:08:40 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -88,3 +88,9 @@ from sklearn.metrics import roc_aus_score
 
 y_scores = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3,
                              method="decision_function")
+roc_aus_score(y_train_5, y_scores)
+
+#다음은 실행하는데 몇 분이 걸릴 수 있음
+from sklearn.ensemble import RanndimForestClassicfier
+
+forest_clf = Random
